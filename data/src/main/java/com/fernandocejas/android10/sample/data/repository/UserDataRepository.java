bce49b56d04c2ddc6828e4b5df0c4e8ec3e237ef
zhengxiaopeng <rocko.zxp@gmail.com>
2015-11-04 23:22:28 +0800

    remove lambda

--- data/src/main/java/com/fernandocejas/android10/sample/data/repository/UserDataRepository.java
+++ data/src/main/java/com/fernandocejas/android10/sample/data/repository/UserDataRepository.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Fernando Cejas Open Source Project
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,20 @@
  */
 package com.fernandocejas.android10.sample.data.repository;
 
+import com.fernandocejas.android10.sample.data.entity.UserEntity;
 import com.fernandocejas.android10.sample.data.entity.mapper.UserEntityDataMapper;
 import com.fernandocejas.android10.sample.data.repository.datasource.UserDataStore;
 import com.fernandocejas.android10.sample.data.repository.datasource.UserDataStoreFactory;
 import com.fernandocejas.android10.sample.domain.User;
 import com.fernandocejas.android10.sample.domain.repository.UserRepository;
+
 import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * {@link UserRepository} for retrieving user data.
@@ -31,34 +36,47 @@ import rx.Observable;
 @Singleton
 public class UserDataRepository implements UserRepository {
 
-  private final UserDataStoreFactory userDataStoreFactory;
-  private final UserEntityDataMapper userEntityDataMapper;
+	private final UserDataStoreFactory userDataStoreFactory;
+	private final UserEntityDataMapper userEntityDataMapper;
 
-  /**
-   * Constructs a {@link UserRepository}.
-   *
-   * @param dataStoreFactory A factory to construct different data source implementations.
-   * @param userEntityDataMapper {@link UserEntityDataMapper}.
-   */
-  @Inject
-  public UserDataRepository(UserDataStoreFactory dataStoreFactory,
-      UserEntityDataMapper userEntityDataMapper) {
-    this.userDataStoreFactory = dataStoreFactory;
-    this.userEntityDataMapper = userEntityDataMapper;
-  }
+	/**
+	 * Constructs a {@link UserRepository}.
+	 *
+	 * @param dataStoreFactory A factory to construct different data source implementations.
+	 * @param userEntityDataMapper {@link UserEntityDataMapper}.
+	 */
+	@Inject
+	public UserDataRepository(UserDataStoreFactory dataStoreFactory,
+	                          UserEntityDataMapper userEntityDataMapper) {
+		this.userDataStoreFactory = dataStoreFactory;
+		this.userEntityDataMapper = userEntityDataMapper;
+	}
 
-  @SuppressWarnings("Convert2MethodRef")
-  @Override public Observable<List<User>> users() {
-    //we always get all users from the cloud
-    final UserDataStore userDataStore = this.userDataStoreFactory.createCloudDataStore();
-    return userDataStore.userEntityList()
-        .map(userEntities -> this.userEntityDataMapper.transform(userEntities));
-  }
+	@SuppressWarnings("Convert2MethodRef")
+	@Override
+	public Observable<List<User>> users() {
+		//we always get all users from the cloud
+		final UserDataStore userDataStore = this.userDataStoreFactory.createCloudDataStore();
 
-  @SuppressWarnings("Convert2MethodRef")
-  @Override public Observable<User> user(int userId) {
-    final UserDataStore userDataStore = this.userDataStoreFactory.create(userId);
-    return userDataStore.userEntityDetails(userId)
-        .map(userEntity -> this.userEntityDataMapper.transform(userEntity));
-  }
+		return userDataStore.userEntityList()
+				.map(new Func1<List<UserEntity>, List<User>>() {
+					@Override
+					public List<User> call(List<UserEntity> userEntities) {
+						return userEntityDataMapper.transform(userEntities);
+					}
+				});
+	}
+
+	@SuppressWarnings("Convert2MethodRef")
+	@Override
+	public Observable<User> user(int userId) {
+		final UserDataStore userDataStore = this.userDataStoreFactory.create(userId);
+		return userDataStore.userEntityDetails(userId)
+				.map(new Func1<UserEntity, User>() {
+					@Override
+					public User call(UserEntity userEntity) {
+						return userEntityDataMapper.transform(userEntity);
+					}
+				});
+	}
 }
--- data/src/main/java/com/fernandocejas/android10/sample/data/repository/datasource/CloudUserDataStore.java
+++ data/src/main/java/com/fernandocejas/android10/sample/data/repository/datasource/CloudUserDataStore.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Fernando Cejas Open Source Project
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,9 @@ package com.fernandocejas.android10.sample.data.repository.datasource;
 import com.fernandocejas.android10.sample.data.cache.UserCache;
 import com.fernandocejas.android10.sample.data.entity.UserEntity;
 import com.fernandocejas.android10.sample.data.net.RestApi;
+
 import java.util.List;
+
 import rx.Observable;
 import rx.functions.Action1;
 
@@ -27,33 +29,37 @@ import rx.functions.Action1;
  */
 public class CloudUserDataStore implements UserDataStore {
 
-  private final RestApi restApi;
-  private final UserCache userCache;
+	private final RestApi restApi;
+	private final UserCache userCache;
 
-  private final Action1<UserEntity> saveToCacheAction =
-      userEntity -> {
-        if (userEntity != null) {
-          CloudUserDataStore.this.userCache.put(userEntity);
-        }
-      };
+	private final Action1<UserEntity> saveToCacheAction = new Action1<UserEntity>() {
+		@Override
+		public void call(UserEntity userEntity) {
+			if (userEntity != null) {
+				CloudUserDataStore.this.userCache.put(userEntity);
+			}
+		}
+	};
 
-  /**
-   * Construct a {@link UserDataStore} based on connections to the api (Cloud).
-   *
-   * @param restApi The {@link RestApi} implementation to use.
-   * @param userCache A {@link UserCache} to cache data retrieved from the api.
-   */
-  public CloudUserDataStore(RestApi restApi, UserCache userCache) {
-    this.restApi = restApi;
-    this.userCache = userCache;
-  }
+	/**
+	 * Construct a {@link UserDataStore} based on connections to the api (Cloud).
+	 *
+	 * @param restApi The {@link RestApi} implementation to use.
+	 * @param userCache A {@link UserCache} to cache data retrieved from the api.
+	 */
+	public CloudUserDataStore(RestApi restApi, UserCache userCache) {
+		this.restApi = restApi;
+		this.userCache = userCache;
+	}
 
-  @Override public Observable<List<UserEntity>> userEntityList() {
-    return this.restApi.userEntityList();
-  }
+	@Override
+	public Observable<List<UserEntity>> userEntityList() {
+		return this.restApi.userEntityList();
+	}
 
-  @Override public Observable<UserEntity> userEntityDetails(final int userId) {
-    return this.restApi.userEntityById(userId)
-        .doOnNext(saveToCacheAction);
-  }
+	@Override
+	public Observable<UserEntity> userEntityDetails(final int userId) {
+		return this.restApi.userEntityById(userId)
+				.doOnNext(saveToCacheAction);
+	}
 }
